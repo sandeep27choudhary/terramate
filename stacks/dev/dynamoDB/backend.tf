5a7799ff398dec5336cf94c75ffd98ca0519a59a
Ekam <ekamdeep@xenonstack.com>
2022-08-23 14:43:43 +0530

    rreverter

--- stacks/dev/dynamoDB/backend.tf
+++ stacks/dev/dynamoDB/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "sudip-test-bucket-269-18"
-    key    = "dynamoDB/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "sudip-test-bucket-269-18"
+#     key    = "dynamoDB/terraform.tfstate"
+#     region = "ap-south-1"
+#   }
+# }
---
+++ stacks/dev/dynamoDB/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "dynamoDB"
+  description = "dynamoDB"
+  id          = "ff07c8ee-3654-4263-8923-f344404dd7b8"
+}
